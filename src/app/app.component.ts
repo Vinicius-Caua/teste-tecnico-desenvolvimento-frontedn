8ffbfee262a9e907abb597debd3d1321f0b44222
Vinicius Caua <vinipg1@hotmail.com>
2025-02-02 12:25:23 -0300

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxMaskDirective, NgxMaskPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
