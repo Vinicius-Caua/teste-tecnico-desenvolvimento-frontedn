28b4de26df75d2fd975e37fa895f3b2e8489320f
Vinicius Caua <vinipg1@hotmail.com>
2025-02-02 23:22:43 -0300

    fix: corrigir cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue-botao": "#FF8A00",
+        "blue-background-form": "#668BB1",
+        "blue-label": "#557EA9",
+        "dark-blue-background": "rgb(2, 3, 129)"
+      }
+    },
   },
   plugins: [],
 }
